ef1516a7b6bb3de7102ae41c37e8369a2f4db4fd
CalebComsa99 <73489946+CalebComsa99@users.noreply.github.com>
2021-09-27 21:14:02 +0300

    Initial commit

---
+++ LAB 1/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        Book discoTitanic = new Book("Disco Titanic");
+        discoTitanic.createNewParagraf("Paragraph 1");
+        discoTitanic.createNewParagraf("Paragraph 2");
+        discoTitanic.createNewParagraf("Paragraph 3");
+        discoTitanic.createNewImagine("Image 1");
+        discoTitanic.createNewParagraf("Paragraph 4");
+        discoTitanic.createNewTabel("Table 1");
+        discoTitanic.createNewParagraf("Paragraph 5");
+    }
+}
